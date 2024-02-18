7dd6a2df6235e1ae96b776d39e46d09c464affbe
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-18 09:20:08 +0100

    min 42-54

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,7 +1,28 @@
+import AnalyticsDashboard from "@/components/AnalyticsDashboard";
 import { analytics } from "@/utils/analytics";
 
 const Page = async () => {
-  const pageview = await analytics.retrieve("pageview", "17/02/2024");
-  return <pre className="text-white">{JSON.stringify(pageview)}</pre>;
+  const TRACKING_DAYS = 7;
+
+  const pageviews = await analytics.retrieveDays("pageview", TRACKING_DAYS);
+
+  const totalPageviews = pageviews.reduce((acc, curr) => {
+    return (
+      acc +
+      curr.events.reduce((acc, curr) => {
+        return acc + Object.values(curr)[0]!;
+      }, 0)
+    );
+  }, 0);
+
+  const avgVisitorsPerDay = (totalPageviews / TRACKING_DAYS).toFixed(1);
+
+  return (
+    <div className="min-h-screen w-full py-12 flex justify-center items-center">
+      <div className="relative w-full max-w-6xl mx-auto text-white">
+        <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} />
+      </div>
+    </div>
+  );
 };
 export default Page;
