9c18198906a4bc7e2c1bb1b1e7d5b47304e9795a
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-19 02:47:21 +0100

    1h09min

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { Card } from "@tremor/react";
+import { analytics } from "@/utils/analytics";
+import { BarChart, Card } from "@tremor/react";
 
 interface AnalyticsDashboardProps {
   avgVisitorsPerDay: string;
+  amtVisitorsToday: number;
+  timeseriesPageviews: Awaited<ReturnType<typeof analytics.retrieveDays>>;
 }
 
-const AnalyticsDashboard = ({ avgVisitorsPerDay }: AnalyticsDashboardProps) => {
+const AnalyticsDashboard = ({
+  avgVisitorsPerDay,
+  amtVisitorsToday,
+  timeseriesPageviews,
+}: AnalyticsDashboardProps) => {
   return (
     <div className="flex flex-col gap-6">
       <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
@@ -18,7 +25,32 @@ const AnalyticsDashboard = ({ avgVisitorsPerDay }: AnalyticsDashboardProps) => {
             {avgVisitorsPerDay}
           </p>
         </Card>
+        <Card className="w-full mx-auto max-w-xs">
+          <p className="text-tremor-default text-dark-tremor-content">
+            Visitors today
+          </p>
+          <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+            {amtVisitorsToday}
+          </p>
+        </Card>
       </div>
+
+      <Card>
+        {timeseriesPageviews ? (
+          <BarChart
+            allowDecimals={false}
+            showAnimation
+            data={timeseriesPageviews.map((day) => ({
+              name: day.date,
+              Visitors: day.events.reduce((acc, curr) => {
+                return acc + Object.values(curr)[0]!;
+              }, 0),
+            }))}
+            categories={["Visitors"]}
+            index="name"
+          />
+        ) : null}
+      </Card>
     </div>
   );
 };
